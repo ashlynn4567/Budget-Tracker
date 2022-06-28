8d5175c6620e41e70eb161804d45268568363c91
Ashley Smith <ashlynn4567@gmail.com>
2022-06-28 13:54:19 -0500

    activated service worker in service-worker.js

--- public/service-worker.js
+++ public/service-worker.js
@@ -26,3 +26,24 @@ self.addEventListener("install", function(e) {
         })
     );
 });
+
+// activate service worker
+self.addEventListener("activate", function(e) {
+    e.waitUntil(
+        caches.keys().then(function (keyList) {
+            let cacheKeeplist = keyList.fileter(function (key) {
+                return key.indexOf(APP_PREFIX);
+            });
+            cacheKeeplist.push(CACHE_NAME);
+
+            return Promise.all(
+                keyList.map(function (key, i) {
+                    if (cacheKeeplist.indexOf(key) === -1) {
+                        console.log("deleting cache : " + keyList[i]);
+                        return caches.delete(keyList[i]);
+                    };
+                })
+            );
+        })
+    );
+});
