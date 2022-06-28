f7d3e86ed71b56e9261ded58a582c6d73f4af69b
Ashley Smith <ashlynn4567@gmail.com>
2022-06-28 13:42:17 -0500

    created offline capability so that user can upload a transaction while offline. when app comes online, it will upload the transaction

--- public/js/idb.js
+++ public/js/idb.js
@@ -29,3 +29,41 @@ function saveRecord(record) {
     // add record to objectStore
     transactionObjectStore.add(record);
 };
+
+// once online connection is reestablished, upload data
+function uploadTransaction() {
+    const transaction = db.transaction(["new_transaction"], "readwrite");
+    const transactionObjectStore = transaction.objectStore("new_transaction");
+
+    // set all records from object store to a variable
+    const getAll = transactionObjectStore.getAll();
+
+    getAll.onsuccess = function () {
+        if (getAll.result.length > 0) {
+            fetch("/api/transaction/bulk", {
+                method: "POST", 
+                body: JSON.stringify(getAll.result),
+                headers: {
+                    Accept: "application/json, test/plain, */*",
+                    "Content-Type": "application/json"
+                }
+            })
+            .then(response => response.json())
+            .then(serverResponse => {
+                if (serverResponse.message) {
+                    throw new Error(serverResponse);
+                };
+                const transaction = db.transaction(["new_transaction", "readwrite"]);
+                const transactionObjectStore = transaction.objectStore("new_transaction");
+
+                // clear store
+                transactionObjectStore.clear();
+                alert("All saved transactions have been uploaded!");
+            })
+            .catch(err => console.log(err));
+        };
+    };
+};
+
+// listen for app to come online
+window.addEventListener("online", uploadTransaction);
