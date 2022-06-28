302f8b761f674b3916e4ac9116c77f1ff2f235fa
Ashley's PC <ashlynn4567@gmail.com>
2022-06-28 18:47:26 -0500

    fixed syntax issue

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use(express.json());
 app.use(express.static("public"));
 
 // connect application to database
-mongoose.connect(process.env.MONGODB_URI, {
+mongoose.connect(MONGODB_URI, {
   useNewUrlParser: true,
   useFindAndModify: false
 });
